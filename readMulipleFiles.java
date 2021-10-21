cfa4efee705d0fbe195c181701f4007006ede775
Matt Caruso <40008209+Matt-Caruso@users.noreply.github.com>
2021-10-21 10:54:44 -0400

    COMMIT ON MASTER

--- readMulipleFiles.java
+++ readMulipleFiles.java
@@ -2,21 +2,33 @@ import java.io.*;
 import java.util.Scanner;
 import java.io.File;  
 import java.io.FileNotFoundException;
-
+//PUT ALL TXT FILES INTO SEP FOLDER --> CHANGE DIRECTORY TO THAT PATH
 public class readMulipleFiles
 {
     public static void main (String[] args)
     {
-        File dir = new File("Dir/ToYour/Files");
+        File dir = new File("C:\\Users\\Matthew Caruso\\Documents\\rwu\\COMSC330\\COMSC-330-Project");
 
         if(dir.isDirectory()) {
            for(File file : dir.listFiles()) {
               if(file.isFile()) {
-                 //do stuff on a file
+               try {
+                  File myObj = new File("file.txt"); //creates file obj
+                  Scanner myReader = new Scanner(myObj);
+                  while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                  }
+                  myReader.close();
+                } catch (FileNotFoundException e) {
+                  System.out.println("An error occurred.");
+                  e.printStackTrace();
+                }
+         
               }
            }
         } else {
-           //do stuff on a file
+           System.out.println("");
         }
     }
 }
