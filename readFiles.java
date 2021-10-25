202b732e16c57e3d4b2283aff16878ff75a7a067
Matt Caruso <40008209+Matt-Caruso@users.noreply.github.com>
2021-10-25 18:11:04 -0400

    10/25

--- readFiles.java
+++ readFiles.java
@@ -3,27 +3,19 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class readMulipleFiles {
+public class readFiles {
 	public static void main(String[] args) throws IOException{  
         File dir = new File("C:\\Users\\Matthew Caruso\\Documents\\rwu\\COMSC330\\COMSC-330-Project\\textFiles");
         File[] files = dir.listFiles();
-        String[] arr = new String[5];//array for characters
         // Fetching all the files
         for (File file : files) {
             if(file.isFile()) {
                 BufferedReader inputStream = null;
-                String line = "hello";
-                char h;
-           
+                String line;
                 try {
                     inputStream = new BufferedReader(new FileReader(file));
                     while ((line = inputStream.readLine()) != null) {
-                     h = line.charAt(line.length()-1);  
-                     //if grade --> switch
-                     //if +/- add/sub .333
-                        //get char at length - 2 --> switch
-                     //put into array
-                     System.out.println(h);
+                        System.out.println(line);
                     }
                 }catch(IOException e) {
                 	System.out.println(e);
--- readLineTest.java
+++ readLineTest.java
@@ -1,7 +1,6 @@
-import java.io.*;
-import java.util.Scanner;
-import java.io.File;  
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class readLineTest
 {
     public static void main (String[] args)
